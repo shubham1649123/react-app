0361eb6b3d81b9134f003efe86be9d3cc2c2583f
Shubham kumar jha <shubhamjha1649@gmail.com>
2025-09-04 21:16:58 +0000

    udated after navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,25 +1,46 @@
 import React from "react";
-import ProptTypes from 'prop-types';
+// import ProptTypes from 'prop-types';
 
 
-export default function Navbar(props){
-    return(
-        <div>
-            <ul>
-                <li>{props.home}</li>
-                <li>{props.about}</li>
-                <li>{props.contact}</li>
-            </ul>
-        </div>
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
-Navbar.ProptTypes={
-    home:ProptTypes.string.isRequired,
-    about:ProptTypes.string.isRequired,
-    contact:ProptTypes.string.isRequired
-}
-// Navbar.defaultProps={
-//     home:"set home",
-//     about:"set about",
-//     contact:"set contact"
-// }
