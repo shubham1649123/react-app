0dabe016fea1ccf7abfc68f9b9d84660c752b353
Shubham kumar jha <shubhamjha1649@gmail.com>
2025-09-04 22:40:04 +0000

    after changing props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Navbar from './components/Navbar'
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <>
-    <Navbar />
+    <Navbar title="To_do app" searchbar={false}/>
+    <Header />
+    <Todos />
+    <Footer />
     </>
   );
 }
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
-// import ProptTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="#">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -17,7 +17,7 @@ export default function Navbar() {
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                                <a className="nav-link" href="#">link</a>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -34,10 +34,10 @@ export default function Navbar() {
                                 <a className="nav-link disabled" aria-disabled="true">Disabled</a>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
+                        {props.searchbar ? (<form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                        </form>) : ""}
                     </div>
                 </div>
             </nav>
